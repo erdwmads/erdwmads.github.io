30be77c080fc1926c6479efeb588f7e2abf0fea6
erdwmads <tsukuyomikoyomiyuki@gmail.com>
2026-06-06 04:01:07 +0900

    Add files via upload

--- assets/js/interface-2046.js
+++ assets/js/interface-2046.js
@@ -1,6 +1,5 @@
 
 (() => {
-  const root = document.documentElement;
   const body = document.body;
 
   if (body.dataset.interface2046 === 'ready') return;
@@ -16,15 +15,22 @@
 
   layer.innerHTML = `
     <div class="ui2046-progress"><span></span></div>
-    <div class="ui2046-corner ui2046-corner-tl"></div>
-    <div class="ui2046-corner ui2046-corner-br"></div>
-    <div class="ui2046-orbit"></div>
 
-    <aside class="ui2046-rail ui2046-left">
-      <strong>ASTROMATERIALS</strong>
-      <div class="ui2046-rail-line"></div>
-      <span>CI1 · ORGUEIL · DOLOMITE</span>
-    </aside>
+    <div class="ui2046-solar-system">
+      <span class="ui2046-kuiper-belt"></span>
+      <span class="ui2046-system-core"></span>
+
+      <span class="ui2046-system-orbit inner" style="--w:18%;--h:11%;--px:45%;--py:-2%;"></span>
+      <span class="ui2046-system-orbit inner" style="--w:25%;--h:16%;--px:-43%;--py:4%;"></span>
+      <span class="ui2046-system-orbit earth" style="--w:33%;--h:22%;--px:37%;--py:-12%;"></span>
+      <span class="ui2046-system-orbit inner" style="--w:42%;--h:29%;--px:-34%;--py:-14%;"></span>
+      <span class="ui2046-system-orbit giant" style="--w:56%;--h:39%;--px:47%;--py:6%;--planet:4.5px;"></span>
+      <span class="ui2046-system-orbit giant" style="--w:68%;--h:49%;--px:-45%;--py:9%;--planet:4px;"></span>
+      <span class="ui2046-system-orbit giant" style="--w:79%;--h:59%;--px:30%;--py:23%;--planet:3.5px;"></span>
+      <span class="ui2046-system-orbit giant" style="--w:89%;--h:68%;--px:-29%;--py:-24%;--planet:3.5px;"></span>
+
+      <span class="ui2046-system-orbit dwarf" style="--w:98%;--h:80%;--px:42%;--py:-31%;"></span>
+    </div>
 
     <aside class="ui2046-rail ui2046-right">
       <strong>${route}</strong>
